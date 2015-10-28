68274bf419525fb009267cd2ea4aaca759f4bb42
Jorrit Poelen <jhpoelen@gmail.com>
2015-10-28 08:29:31 -0700

    rebuild

--- static/bundle.js
+++ static/bundle.js
@@ -241,7 +241,7 @@ var addDownloadAsEOLIdsLink = function (pageIds) {
       saveStatus = download.appendChild(document.createElement("span"));
       saveStatus.setAttribute('id', 'saveStatus');
     }
-    saveStatus.innerHTML = ' collection saving...';
+    saveStatus.innerHTML = ' Collection saving...';
     var pageIds = JSON.parse(document.querySelector('#download').dataset.eolPageIds).map(function(item) { return parseInt(item); });
     var apiKey = document.querySelector('#apiKey').value;
     var title = document.querySelector('#collectionTitle').value;
@@ -256,7 +256,7 @@ var addDownloadAsEOLIdsLink = function (pageIds) {
     var limitedPageIds = pageIds.slice(0, maxElements);
     taxon.saveAsCollection(function(collectionId) {
       var collectionURL = 'http://eol.org/collections/' + collectionId;
-      var saveStatusHTML = ' collection saved at <a href="' + collectionURL + '">' + collectionURL + '</a>.';
+      var saveStatusHTML = ' Collection saved at <a href="' + collectionURL + '">' + collectionURL + '</a>.';
       if (!collectionId) {
         saveStatusHTML = ' Failed to save collection. Bummer! This is probably a <a href="https://github.com/EOL/tramea/issues/35">known issue</a> that prevents saving > ' + maxCollectionItems + ' items to a EOL checklist. However, if your list contains only a few items and you are seeing this message, please check <a href="https://github.com/jhpoelen/effechecka/issues/">our open issues</a> first, before reporting a new one.';
       }
