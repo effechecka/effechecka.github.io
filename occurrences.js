4d7e690a2141e3e0ac93bec8a7804ffdeb6168ea
Jorrit Poelen <jhpoelen@gmail.com>
2016-06-01 13:45:09 -0700

    add webhook test input field related to https://github.com/gimmefreshdata/freshdata/issues/27

--- occurrences.js
+++ occurrences.js
@@ -232,30 +232,35 @@ occurrences.select = function (selector) {
     var subscribeButton = document.querySelector('#effechecka-subscribe');
     if (subscribeButton) {
         subscribeButton.addEventListener('click', function (event) {
-            var emailInput = document.querySelector('#effechecka-email');
-            if (emailInput && !emailInput.validity.typeMismatch) {
-                var emailAddress = emailInput.value;
-                var dataFilter = selector.getDataFilter();
-                dataFilter['subscriber'] = 'mailto:' + emailAddress;
-                var subscribeUrl = util.createRequestURL(dataFilter, 'subscribe');
-                var req = xhr();
-                if (req !== undefined) {
-                    req.open('GET', subscribeUrl, true);
-                    req.onreadystatechange = function () {
-                        if (req.readyState === 4) {
-                            if (req.status === 200) {
-                                var subscribeStatus = document.querySelector('#effechecka-subscribe-status');
-                                if (subscribeStatus) {
-                                    util.removeChildren('#effechecka-subscribe-status');
-                                    var status = subscribeStatus.appendChild(document.createElement('span'));
-                                    status.textContent = "[" + emailAddress + "] is now subscribed: you should receive a confirmation email shortly.";
+
+            function subscribe(inputSelector, prefix) {
+                var emailInput = document.querySelector(inputSelector);
+                if (emailInput && !emailInput.validity.typeMismatch) {
+                    var emailAddress = emailInput.value;
+                    var dataFilter = selector.getDataFilter();
+                    dataFilter['subscriber'] = prefix + emailAddress;
+                    var subscribeUrl = util.createRequestURL(dataFilter, 'subscribe');
+                    var req = xhr();
+                    if (req !== undefined) {
+                        req.open('GET', subscribeUrl, true);
+                        req.onreadystatechange = function () {
+                            if (req.readyState === 4) {
+                                if (req.status === 200) {
+                                    var subscribeStatus = document.querySelector('#effechecka-subscribe-status');
+                                    if (subscribeStatus) {
+                                        util.removeChildren('#effechecka-subscribe-status');
+                                        var status = subscribeStatus.appendChild(document.createElement('span'));
+                                        status.textContent = "[" + emailAddress + "] is now subscribed: you should receive a confirmation email shortly.";
+                                    }
                                 }
                             }
                         }
                     }
+                    req.send(null);
                 }
-                req.send(null);
             }
+            subscribe('#effechecka-email','mailto:');
+            subscribe('#effechecka-webhook','');
         }, false);
     }
 
